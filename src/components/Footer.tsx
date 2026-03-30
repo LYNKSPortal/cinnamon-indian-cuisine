78e1f33e72dea7f9825c89d4e29c412076a1bf6a
User <user@Mac.lan>
2026-03-30 01:01:04 +0100

    Update Google Maps link to new location
    - Change Google Maps iframe src from embed URL to new maps link
    - Update to: https://maps.app.goo.gl/Xe5aeRCVcVF4J6189
    - Maintain same iframe styling and functionality
    - Keep responsive design and accessibility features

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -121,7 +121,7 @@ export default function Footer() {
               <h4 className="mb-4">Find Us</h4>
               <div className="bg-white/10 rounded-lg overflow-hidden mb-4">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5170.123456789!2d-4.617123456789!3d54.083123456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s31%20Malew%20Street%2C%20Castletown%2C%20IM9%201AD!2sCinnamon%20Indian%20Cuisine!5e0!3m2!1sen!2sim!4v1234567890"
+                  src="https://maps.app.goo.gl/Xe5aeRCVcVF4J6189"
                   width="100%"
                   height="200"
                   style={{ border: 0 }}
